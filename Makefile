ea02da90123121c1e1a9389b211d6811d287fae4
izabera <izaberina@gmail.com>
2014-12-26 19:15:40 +0100

    tee + removed asm stuff

--- Makefile
+++ Makefile
@@ -1,19 +1,13 @@
-CC=diet gcc
-CFLAGS=-Wall -Os -s -fno-inline -nostdinc
+CC = gcc
+CFLAGS = -Wall -O2 -march=native
 
 all:
-	$(CC) $(CFLAGS) -o bin/echo -march=native src/echo.c
-	#$(CC) $(CFLAGS) -o bin/echo -march=native src/echo.c
-	$(CC) $(CFLAGS) -o bin/cat -march=native src/cat.c
-	$(CC) $(CFLAGS) -o bin/rev -march=native src/rev.c
-	#$(CC) $(CFLAGS) -o bin/true -march=native src/true.c
-	#$(CC) $(CFLAGS) -o bin/false -march=native src/false.c
-	nasm -f bin -o bin/true src/true.asm
-	chmod +x bin/true
-	nasm -f bin -o bin/false src/false.asm
-	chmod +x bin/false
-	nasm -f bin -o bin/helloworld src/helloworld.asm
-	chmod +x bin/helloworld
+	#$(CC) $(CFLAGS) -o bin/echo src/echo.c
+	#$(CC) $(CFLAGS) -o bin/cat src/cat.c
+	$(CC) $(CFLAGS) -o bin/tee src/tee.c
+	$(CC) $(CFLAGS) -o bin/true src/true.c
+	$(CC) $(CFLAGS) -o bin/false src/false.c
 
 clean:
 	rm -rf bin/*
+
---
+++ src/tee.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+/* tee */
+
+/* any option that's not -a or -i is treated as files */
+
+int main (int argc, char **argv) {
+  FILE *output[argc];
+  int i, exitcode = 0, opt;
+  bool append = false, ignoresigint = false;
+
+  while ((opt = getopt(argc, argv, "ai")) != -1) {
+    switch (opt) {
+      case 'a':
+        append = true;
+        break;
+      case 'i':
+        signal(SIGINT, SIG_IGN);
+        break;
+      default:
+        fprintf(stderr, "Usage: %s [−ai] [file...]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+
+  for (i = optind; i < argc; i++) {
+    if (append) output[i] = fopen(argv[i], "a");
+    else output[i] = fopen(argv[i], "w");
+    if (output[i]) {
+      setvbuf(output[i], NULL, _IONBF, 0);
+    }
+    else {
+      fprintf(stderr, "ERROR: %s\n", strerror(errno));
+      exitcode = 1;
+    }
+  }
+
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 4096
+#endif
+  char buffer[BUFFER_SIZE];
+  size_t numbytes;
+
+  while (1) {
+    numbytes = fread(buffer, sizeof(unsigned char), BUFFER_SIZE, stdin);
+    for (i = optind; i < argc; i++) 
+      if (output[i]) fwrite(buffer, sizeof(unsigned char), numbytes, output[i]);
+    fwrite(buffer, sizeof(unsigned char), numbytes, stdout);
+    if (numbytes < BUFFER_SIZE) break;
+  }
+  for (i = optind; i < argc; i++) 
+    if (output[i]) fclose(output[i]);
+  return exitcode;
+}
+
