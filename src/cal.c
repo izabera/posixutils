1cc3871fee116a07101b1474eca9ea480751e580
izabera <izaberina@gmail.com>
2014-12-29 21:14:37 +0100

    tmp

--- src/cal.c
+++ src/cal.c
@@ -1,31 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 /* cal */
 
-void printheader (struct tm *time) {
-  char buffer[80];
-  /* strftime thinks months start from 0 */
-  time->tm_mon--;
-  strftime(buffer, 80, "   %B %Y", time);
-  time->tm_mon++;
-  puts(buffer);
+void printheader (struct tm *time, bool printyear) {
+  char buffer[22], spaces[10];
+  int i;
+  size_t output;
+  if (printyear) output = strftime(buffer, 80, "%B %Y", time);
+  else output = strftime(buffer, 80, "%B", time);
+  for (i = 0; i < (21-output)/2; i++) spaces[i] = ' ';
+  spaces[i] = 0;
+  printf("%s%s\n", spaces, buffer);
 }
 
-#define draw()                                                                \
+#define isleap(year) ((!((year)%400)) || (((year)%100) && (!((year)%4))))
+#define draw() {                                                              \
+          for (i = 1; i <= monthlen[time->tm_mon]; i++)                       \
+            array[i+shift] = i;                                 \
           for (i = 0; i < 6; i++)                                             \
-            printf("%2d %2d %2d %2d %2d %2d %2d\n", array[i], array[i+1],     \
-              array[i+2], array[i+3], array[i+4], array[i+5], array[i+6]);
+            printf("%2d %2d %2d %2d %2d %2d %2d\n",   array[i*6+0],           \
+            array[i*6+1], array[i*6+2], array[i*6+3], array[i*6+4],           \
+            array[i*6+5], array[i*6+6]);                                      \
+        }
 
 void printmonth (struct tm *time) {
-  int array[42] = {0}, i = 0,
-      shift[] = { 0, 3, 3, 6, 1, 4, 6, 2, 5, 0, 3, 5 },
+  int array[42] = {0}, i, j, shift,
       monthlen[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-  if (isleap(time->tm_year)) {
-    shift[] = { 0, 3, 4, 0, 2, 5, 0, 3, 6, 1, 4, 6 };
-    monthlen[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-  }
+  char buffer[2];
+  strftime(buffer, 2, "%w", time);
+  shift = buffer[0] - '0';
+  if (isleap(time->tm_year)) monthlen[1] = 29;
 
   if (time->tm_year > 1752-1900) {
     draw();
@@ -51,6 +58,7 @@ void printmonth (struct tm *time) {
 
 int main (int argc, char *argv[]) {
   struct tm *local_time;
+  bool printall = false;
   time_t unix_time;
   time(&unix_time);
   local_time = localtime(&unix_time);
@@ -59,10 +67,10 @@ int main (int argc, char *argv[]) {
       break;
     case 2:
       local_time->tm_year = (unsigned) atoi(argv[1]);
-      local_time->tm_mon = 0;
+      printall = true;
       break;
     case 3:
-      local_time->tm_mon = (unsigned) atoi(argv[1]);
+      local_time->tm_mon = (unsigned) atoi(argv[1]) - 1;
       local_time->tm_year = (unsigned) atoi(argv[2]);
       break;
     default:
@@ -73,16 +81,19 @@ int main (int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s [[month] year]\n", argv[0]);
     exit(EXIT_FAILURE);
   }
+
   local_time->tm_mday = 1;
-  local_time->tm_year -= 1900;
+  /*local_time->tm_year -= 1900;*/
   char days[] = "Su Mo Tu We Th Fr Sa";
-  if (!local_time->tm_mon) while (++local_time->tm_mon < 13) {
-    printheader(local_time);
-    puts(days);
-    printmonth(local_time);
+  if (printall) {
+    do {
+      printheader(local_time, false);
+      puts(days);
+      printmonth(local_time);
+    } while (++local_time->tm_mon < 12);
   }
   else {
-    printheader(local_time);
+    printheader(local_time, true);
     puts(days);
     printmonth(local_time);
   }
