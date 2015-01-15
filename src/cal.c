94abe6556796aa067fe85fa59f3b6eb5964d0b28
izabera <izaberina@gmail.com>
2015-01-15 08:26:31 +0100

    save

--- src/cal.c
+++ src/cal.c
@@ -13,7 +13,11 @@
 void printheader (struct tm *time, bool printyear) {
   char buffer[22], spaces[10];
   size_t i, output;
-  if (printyear) output = strftime(buffer, 80, "%B %Y", time);
+  if (printyear) {
+    time->tm_year-=1900;
+    output = strftime(buffer, 80, "%B %Y", time);
+    time->tm_year+=1900;
+  }
   else output = strftime(buffer, 80, "%B", time);
   for (i = 0; i < (21-output)/2; i++) spaces[i] = ' ';
   spaces[i] = 0;
@@ -45,18 +49,18 @@ void printmonth (struct tm *time) {
   int array[42] = {0}, i, j, shift,
       monthlen[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-  shift = dow(time->tm_year + 1900, time->tm_mon + 1, time->tm_mday);
+  shift = dow(time->tm_year, time->tm_mon, time->tm_mday);
   if (isleap(time->tm_year)) monthlen[1] = 29;
 
-  if (time->tm_year > 1752-1900) {
+  if (time->tm_year > 1752) {
     draw();
   }
-  else if (time->tm_year == 1752-1900) {
+  else if (time->tm_year == 1752) {
     if (time->tm_mon > 9) {
      /* gregorian calendar starts here */
       draw();
     }
-    else if (time->tm_mon == 8) {
+    else if (time->tm_mon == 9) {
       puts("       1  2 14 15 16");
       puts("17 18 19 20 21 22 23");
       puts("24 25 26 27 28 29 30");
@@ -91,52 +95,36 @@ int main (int argc, char *argv[]) {
   time(&unix_time);
   local_time = localtime(&unix_time);
 
-  /* why most of this is idiotic:
-   *
-   * 1. strftime thinks year 0 is 1900
-   * we have to remove 1900 if the user inputs a parameter
-   * add 1900 and remove it if no parameter was inserted
-   *
-   * 2. strftime thinks jan = 0
-   * which is not too bad compared to the workaround required for the years
-   * 
-   * 3. strftime only uses the gregorian calendar
-   * we need to hand convert the days before september 1752
-   *
-   * 4. the tm structure doesn't contain an actual date
-   * we can't generate it with mktime to know the day of the week
-   * it fails on 32 bit machines when year < 1912    (year 2038 bug) */
+  /* saving the date to a human readable format */
 
   switch (argc) {
     case 1:
       local_time->tm_year += 1900;
+      local_time->tm_mon++;
       break;
     case 2:
       local_time->tm_year = (unsigned) atoi(argv[1]);
+      local_time->tm_mon = 1;
       printall = true;
       break;
     case 3:
-      local_time->tm_mon = (unsigned) atoi(argv[1]) - 1;
+      local_time->tm_mon = (unsigned) atoi(argv[1]);
       local_time->tm_year = (unsigned) atoi(argv[2]);
       break;
     default:
       fprintf(stderr, "Usage: %s [[month] year]\n", argv[0]);
       exit(EXIT_FAILURE);
   }
-  if (local_time->tm_mon > 12 || local_time->tm_year > 9999) {
+  if (local_time->tm_mon > 12 || local_time->tm_year < 0 || local_time->tm_year > 9999) {
     fprintf(stderr, "Usage: %s [[month] year]\n", argv[0]);
     exit(EXIT_FAILURE);
   }
-
-  local_time->tm_mday = 1;
-  local_time->tm_year -= 1900;
 
   char days[] = "Su Mo Tu We Th Fr Sa";
   /*char days[22];*/
   /*generate_days();*/
 
   if (printall) {
-    local_time->tm_mon = 0;
     do {
       printheader(local_time, false);
       puts(days);
